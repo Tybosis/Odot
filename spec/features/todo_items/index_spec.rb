c5e155ae97ced0e659298f37b1d74c47bf8d6fa2
Tyler Pottle <tyler.pottle@gmail.com>
2015-01-27 14:36:34 -0800

    Added test for creating todo items and associated code

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Viewing todo items" do
   let!(:todo_list) { TodoList.create(title: "grocery list", description: "groceries")}
+
   def visit_todo_list(list)
     visit "/todo_lists"
     within "#todo_list_#{list.id}" do
