05cdc277d53fa442cbefc9ed746871eabafc124d
Tyler Pottle <tyler.pottle@gmail.com>
2015-02-13 14:42:22 -0800

    Add password resets & email configuration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,7 @@ describe User do
       User.create(valid_attributes)
     end
 
-    it "requires an email" do 
+    it "requires an email" do
       expect(user).to validate_presence_of(:email)
     end
 
@@ -39,7 +39,7 @@ describe User do
       user.email = "jason"
       expect(user).to_not be_valid
     end
-    
+
   end
 
   describe "#downcase_email" do
@@ -53,8 +53,22 @@ describe User do
     it "downcases an email before saving" do
       user = User.new(valid_attributes)
       user.email = "MIKE@TEAMTREEHOUSE.COM"
-      expect(user.save).to be_true
+      expect(user.save).to be_truthy
       expect(user.email).to eq("mike@teamtreehouse.com")
+    end
+  end
+
+  describe "#generate_password_reset_token!" do
+    let(:user) { create(:user) }
+
+    it "changes the password_reset_token attribute" do
+      expect{ user.generate_password_reset_token!}.
+        to change{user.password_reset_token}
+    end
+
+    it "calls SecureRandom.urlsafe_base64 to generate the password_reset_token" do
+      expect(SecureRandom).to receive(:urlsafe_base64)
+      user.generate_password_reset_token!
     end
   end
 end
